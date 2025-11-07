248d7ad73c573ea294d2bd6cd140e00bcbd12692
B-l-i-n-k-e-r <vinniemariba2004@gmail.com>
2025-11-07 11:02:57 +0300

    lets see

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -6,7 +6,7 @@ import CanvasLoader from '../Loader';
 
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
-
+ 
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
@@ -21,8 +21,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.4 : 0.45}
+        position={isMobile ? [0, -2, 0] : [0, 0, 0]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
